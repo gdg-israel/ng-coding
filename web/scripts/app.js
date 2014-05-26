55904b7a3dbca65aba1cf287fc778f2d12fe0d08
Uri Shaked <uri@salsa4fun.co.il>
2014-05-26 22:42:12 +0300

    feat(*): integrate ui-router, build primary screens

--- web/scripts/app.js
+++ web/scripts/app.js
@@ -1,1 +1,17 @@
-angular.module('gdg-israel.ng-coding', []);
+angular
+	.module('gdg-israel.ng-coding', ['ui.router'])
+	.config(function ($stateProvider) {
+		$stateProvider
+			.state('index', {
+				url: "",
+				templateUrl: "partials/login.html"
+			})
+			.state('leaderboard', {
+				url: "/leaderboard",
+				templateUrl: "partials/leaderboard.html"
+			})
+			.state('contributions', {
+				url: "/contributions",
+				templateUrl: "partials/contributions.html"
+			});
+	});
