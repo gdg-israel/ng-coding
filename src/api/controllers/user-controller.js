bd80c67ec6bd95628cba546314dcad8420586828
Ran Mizrahi <ran@cocycles.com>
2014-05-29 21:28:05 +0300

    added current api

--- src/api/controllers/user-controller.js
+++ src/api/controllers/user-controller.js
@@ -1,1 +1,14 @@
 'use strict';
+
+module.exports = {
+    currentUser: function(req, res) {
+        var user = req.user;
+
+        if (!req.user) {
+            res.unauthorized('user is not logged-in');
+            return;
+        }
+
+        res.ok(req.user);
+    }
+};
