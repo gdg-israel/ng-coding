34b167470bb183d01e18f46da7486c005d60e639
Uri Shaked <uri@salsa4fun.co.il>
2014-05-31 02:27:26 +0300

    styles(*): fix indent, jshint issues

--- web/scripts/leaderboard.js
+++ web/scripts/leaderboard.js
@@ -1,20 +1,21 @@
 'use strict';
 
 angular.module('ngCoding.leaderboard', [])
-		.controller('LeaderboardCtrl', function ($scope, User, $interval) {
-        function update() {
-            User.all()
-					.then(function (users) {
-						$scope.usersScore = users;
-						$scope.activities = User.pollActivities();
-						$scope.gravatarUrl = function (hash) {
-								return User.getGravatarUrl(hash, 40);
-						}
-					});
-				}
-				update();
-				var autoUpdateInterval = $interval(update, 30000);
-				$scope.$on('$destory', function () {
-					$interval.cancel(autoUpdateInterval);
+	.controller('LeaderboardCtrl', function ($scope, User, $interval) {
+		function update() {
+			User.all()
+				.then(function (users) {
+					$scope.usersScore = users;
+					$scope.activities = User.pollActivities();
+					$scope.gravatarUrl = function (hash) {
+						return User.getGravatarUrl(hash, 40);
+					};
 				});
+		}
+
+		update();
+		var autoUpdateInterval = $interval(update, 30000);
+		$scope.$on('$destory', function () {
+			$interval.cancel(autoUpdateInterval);
+		});
 	});
--- web/scripts/services.js
+++ web/scripts/services.js
@@ -1,47 +1,52 @@
 'use strict';
 angular.module('ngCoding.services', [])
-  .factory('User', function ($http, $timeout, $q) {
-        var usersBuffer = 0;
-        var usersCollection = [];
-        var activities = {};
-        var pollingInitialized = false;
-        var pollActivities = function () {
-            angular.forEach(usersCollection, function (user) {
-                $http.get('https://api.github.com/users/' + user.username + '/events').success(function (res) {
-                    var userActivities = res;
-                    var lastActivity = userActivities[0];
-                    activities[user._id] = {content: lastActivity.type +  ' @ Repo ' + lastActivity.repo.name, time: moment(lastActivity.created_at).format('MMMM Do YYYY, h:mm:ss a')};
-                });
-            });
-            $timeout(pollActivities, 2000);
-        };
-        return {
-            isLoggedIn: function () {
-                return angular.isDefined(localStorage.getItem('userId'));
-            },
-            current: function () {
-                return $http.get('/user/current');
-            },
-            all: function () {
-              var deferred = $q.defer();
-                $http.get('/leaderboard').then(function (res) {
-                    deferred.resolve(usersCollection = res.data.payload)
-                });
-                return deferred.promise;
-            },
-            getGravatarUrl: function (uId, size) {
-                var suffix = (angular.isDefined(size))? 's=' + size: '';
-                return 'http://www.gravatar.com/avatar/' + uId + '?' + suffix;
-            },
-            getProfileUrl: function (username) {
-                return 'https://github.com/' + username;
-            },
-            pollActivities: function() {
-                if(!pollingInitialized){
-                    pollingInitialized = true;
-                    pollActivities();
-                }
-                return activities;
-            },
-        };
-    });
+	.factory('User', function ($http, $timeout, $q) {
+		var usersCollection = [];
+		var activities = {};
+		var pollingInitialized = false;
+		var pollActivities = function () {
+			angular.forEach(usersCollection, function (user) {
+				$http.get('https://api.github.com/users/' + user.username + '/events').success(function (res) {
+					var userActivities = res;
+					var lastActivity = userActivities[0];
+					/* jshint camelcase:false */
+					activities[user._id] = {
+						content: lastActivity.type + ' @ Repo ' + lastActivity.repo.name,
+						time: lastActivity.created_at
+					};
+					/* jshint camelcase:true */
+				});
+			});
+			$timeout(pollActivities, 2000);
+		};
+		return {
+			isLoggedIn: function () {
+				return angular.isDefined(localStorage.getItem('userId'));
+			},
+			current: function () {
+				return $http.get('/user/current');
+			},
+			all: function () {
+				var deferred = $q.defer();
+				$http.get('/leaderboard').then(function (res) {
+					usersCollection = res.data.payload;
+					deferred.resolve(usersCollection);
+				});
+				return deferred.promise;
+			},
+			getGravatarUrl: function (uId, size) {
+				var suffix = (angular.isDefined(size)) ? 's=' + size : '';
+				return 'http://www.gravatar.com/avatar/' + uId + '?' + suffix;
+			},
+			getProfileUrl: function (username) {
+				return 'https://github.com/' + username;
+			},
+			pollActivities: function () {
+				if (!pollingInitialized) {
+					pollingInitialized = true;
+					pollActivities();
+				}
+				return activities;
+			}
+		};
+	});
