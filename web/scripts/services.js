b7cc244e88f28de84b7d146630ea79be07bbb35e
Uri Shaked <uri@salsa4fun.co.il>
2014-05-30 12:59:15 +0300

    style(services): fix indent, jshint issues

--- web/scripts/services.js
+++ web/scripts/services.js
@@ -1,27 +1,27 @@
 'use strict';
 angular.module('ngCoding.services', [])
-  .factory("User", function ($http) {
-      var usersBuffer = 0;
-      return {
-          isLoggedIn: function () {
-            return angular.isDefined(localStorage.getItem("userId"))
-          },
-          current: function () {
-              return $http.get("/user/current")
-          },
-          all: function () {
-            var promise = $http.get("user/all?next="+usersBuffer)
-             promise.finally(function () {
-              usersBuffer+=10;
-            })
-            return promise
-          },
-          getGravatarUrl: function (u_id, size) {
-            var suffix = (angular.isDefined(size))? "s=" + size: "";
-            return "http://www.gravatar.com/avatar/" + u_id + "?" + suffix
-          },
-          getProfileUrl: function (username) {
-            return "https://github.com/" + username;
-          }
-      }
-  })
+	.factory('User', function ($http) {
+		var usersBuffer = 0;
+		return {
+			isLoggedIn: function () {
+				return angular.isDefined(localStorage.getItem('userId'));
+			},
+			current: function () {
+				return $http.get('/user/current');
+			},
+			all: function () {
+				var promise = $http.get('user/all?next=' + usersBuffer);
+				promise.finally(function () {
+					usersBuffer += 10;
+				});
+				return promise;
+			},
+			getGravatarUrl: function (hash, size) {
+				var suffix = (angular.isDefined(size)) ? ('s=' + size) : '';
+				return 'http://www.gravatar.com/avatar/' + hash + '?' + suffix;
+			},
+			getProfileUrl: function (username) {
+				return 'https://github.com/' + username;
+			}
+		};
+	});
