d9188f90bbe86f34602b0e5a1ab82f65054b0eb5
Uri Shaked <uri@salsa4fun.co.il>
2014-05-30 13:03:24 +0300

    style(leaderboard): fix indent, jshint issues

--- web/scripts/leaderboard.js
+++ web/scripts/leaderboard.js
@@ -2,14 +2,21 @@
 
 angular.module('ngCoding.leaderboard', [])
 	.controller('LeaderboardCtrl', function ($scope, User) {
-			User.all().success(function (data, status) {
-				$scope.usersScore = users;
-			}).error(function (data, status) {
-				console.log(data)
+		User.all()
+			.success(function (data) {
+				$scope.usersScore = data.users;
+			}).error(function () {
 				$scope.usersScore = [
-				{userId:551, username:"liorr", score:551, gravatarUrl:User.getGravatarUrl("92ccd542327064bf08d3797c3f291329", 40), lastActivity:{content:"Commited to angular-fire", time:"14:10"}}
-				]
-
-
+					{
+						userId: 551,
+						username: 'liorr',
+						score: 551,
+						gravatarUrl: User.getGravatarUrl('92ccd542327064bf08d3797c3f291329', 40),
+						lastActivity: {
+							content: 'Commited to angular-fire',
+							time: '14:10'
+						}
+					}
+				];
 			});
-	})
+	});
