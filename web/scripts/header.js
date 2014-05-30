0eef51d4be62f287ec128ff8e7901afc5c93b632
Uri Shaked <uri@salsa4fun.co.il>
2014-05-30 12:41:28 +0300

    style(header): fix indent, jshint issues, remove console.log()

--- web/scripts/header.js
+++ web/scripts/header.js
@@ -1,19 +1,16 @@
 'use strict';
 
-ngCoding.controller('HeaderCtrl', function($scope, $window, User) {
-		User.current().success(function (data,status) {
-			console.log(data)
+ngCoding.controller('HeaderCtrl', function ($scope, $window, User) {
+	User.current()
+		.success(function (data) {
 			var user = data.payload;
-			localStorage.setItem("userId",user.userId)
-			localStorage.setItem("username", user.username);
+			localStorage.setItem('userId', user.userId);
+			localStorage.setItem('username', user.username);
 			$scope.user = user;
 			$scope.userLoggedIn = User.isLoggedIn();
-
-		}).error(function (user) {
-			delete localStorage.userId
-			delete localStorage.username
-		$window.location.href = '/'
-			console.log("redirecting")
-			$scope.user = {username:"TestUser", score:50};
-		})
+		}).error(function () {
+			delete localStorage.userId;
+			delete localStorage.username;
+			$window.location.href = '/';
+		});
 });
